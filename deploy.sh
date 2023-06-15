c9e10e7b06ea483619366804c8b34f097e544db6
zengdj <zdj&5914>
2023-06-15 11:37:28 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env sh
- 
 # 确保脚本抛出遇到的错误
 set -e
- 
-# 生成静态文件
-npm run build
- 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
- 
-# 如果是发布到自定义域名
-# echo 'www.zengdaojun.com' > CNAME
 
- 
-# 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:USERNAME/USERNAME.github.io.git master
- 
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
- 
-cd -
+# 百度链接推送
+# curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+# rm -rf urls.txt # 删除文件
+
+#push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+#commit_info=`git describe --all --always --long`
+#dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+#push_branch=gh-pages # 推送的分支
+
+# 生成静态文件
+#npm run build
+
+# 进入生成的文件夹
+#cd $dist_path
+
+#git init
+#git add -A
+#git commit -m "deploy, $commit_info"
+#git push -f $push_addr HEAD:$push_branch
+
+#cd -
+#rm -rf $dist_path
